4fc9f8b6b9e21fbc2dc63a694fb9d2545b6ef35a
B-Abraham <20190315@ce.pucmm.edu.do>
2020-11-13 18:21:05 -0400

    Agregadas las clases Company, CompanyRequest y EmployeeRequest

---
+++ ProyectoFinalP6/src/log/Company.java
@@ -0,0 +1,89 @@
+package log;
+
+public class Company {
+	private String id;
+	private String name;
+	private String area;
+	private String address;
+	private String province;
+	private String city;
+	private String phoneNumber;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getArea() {
+		return area;
+	}
+	
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getProvince() {
+		return province;
+	}
+	
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Company(String id, String name, String area, String address, String province, String city,
+			String phoneNumber, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.area = area;
+		this.address = address;
+		this.province = province;
+		this.city = city;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+}
