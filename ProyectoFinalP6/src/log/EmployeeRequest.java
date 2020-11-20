15948e29716a430ce87c4cfaefbaf885612b0902
B-Abraham <20190315@ce.pucmm.edu.do>
2020-11-19 23:31:13 -0400

    Arregladas las clases CompanyRequest y EmployeeRequest

--- ProyectoFinalP6/src/log/EmployeeRequest.java
+++ ProyectoFinalP6/src/log/EmployeeRequest.java
@@ -1,10 +1,16 @@
 package log;
 
+import java.util.List;
+
 public class EmployeeRequest {
 	private String id;
 	private Person applicant;
 	private Boolean status;
 	private float minSalary;
+	private List<String> languages;
+	private int workingHours;
+	private boolean moveAv;
+	private boolean drivingLicense;
 	
 	public String getId() {
 		return id;
@@ -38,11 +44,48 @@ public class EmployeeRequest {
 		this.minSalary = minSalary;
 	}
 
-	public EmployeeRequest(String id, Person applicant, Boolean status, float minSalary) {
+	public List<String> getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(List<String> languages) {
+		this.languages = languages;
+	}
+
+	public int getWorkingHours() {
+		return workingHours;
+	}
+
+	public void setWorkingHours(int workingHours) {
+		this.workingHours = workingHours;
+	}
+
+	public boolean isMoveAv() {
+		return moveAv;
+	}
+
+	public void setMoveAv(boolean moveAv) {
+		this.moveAv = moveAv;
+	}
+
+	public boolean isDrivingLicense() {
+		return drivingLicense;
+	}
+
+	public void setDrivingLicense(boolean drivingLicense) {
+		this.drivingLicense = drivingLicense;
+	}
+
+	public EmployeeRequest(String id, Person applicant, Boolean status, float minSalary, List<String> languages,
+			int workingHours, boolean moveAv, boolean drivingLicense) {
 		super();
 		this.id = id;
 		this.applicant = applicant;
 		this.status = status;
 		this.minSalary = minSalary;
+		this.languages = languages;
+		this.workingHours = workingHours;
+		this.moveAv = moveAv;
+		this.drivingLicense = drivingLicense;
 	}
 }
